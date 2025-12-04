fb541fd1aa14009ec16005002fa1979626144270
devkhizerahmad <devkhizerahmad@gmail.com>
2025-12-04 17:46:26 +0500

    password generator updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: {
+    '@tailwindcss/postcss': {}, // Use the correct, installed package
+    // 'tailwindcss': {}, // Remove or comment out the old package
+    'autoprefixer': {},
+  },
 };
--- src/components/passwordGenerator/PasswordApp.jsx
+++ src/components/passwordGenerator/PasswordApp.jsx
@@ -1,42 +1,73 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useRef} from "react";
 
 function PasswordApp() {
-
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
+  //ref hook
+  const passwordRef = useRef(null);
+
   const passwordGenerator = useCallback(() => {
-    const pass = ""
-    const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(numberAllowed) str += "0123458789";
-    if(charAllowed) str += "!@#$%^&*()_+";
-    for (let index = 1; index < length; index++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123458789";
+    if (charAllowed) str += "!@#$%^&*()_+"; 
+    for (let index = 1; index <= length; index++) {
+      let char = Math.floor(Math.random() * str.length +1);
       pass += str.charAt(char);
     }
     setPassword(pass);
     console.log("Password Generated", pass);
-    
   }, [length, numberAllowed, charAllowed, setPassword]);
-
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
-    <div>
-      <h1>Password Generator</h1>
-      <div className="mb-4 flex items-center justify-between">
-        <label className="flex items-center justify-between w-64 bg-blue-400 rounded-lg px-2 py-1 border-2 border-b-black">
-          Password Length:
-          <input className="ml-2 w-16 bg-gray-200 text-center border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 ring-purple-600 font-mono text-lg focus:ring-purple-600"
-            type="number"
-            value={password}
-            onChange={(e) => setLength(e.target.value)}
-          />
-        </label>
+      <>
+      <div className="flex items-center justify-between border-2 border-black rounded-lg px-2 py-1">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3 text-black rounded-lg"
+          placeholder="Password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button className="outline-none px-3 py-0.5 bg-blue-500 shrink-0">Copy</button>
       </div>
-    </div>
+      <div className="flex items-center justify-between mt-4">
+        <input 
+        type="range" 
+        max={100}
+        value={length}
+        className="cursor-pointer h-1 bg-gray-200 rounded-lg appearance-none focus:outline-none focus:ring-2 ring-purple-600"
+        onChange={(e) => setLength(e.target.value)}
+         />
+         <label className="text-black ml-2">Length: {length}</label>
+         <input 
+         type="checkbox"
+          id="numberInput"
+          className="mr-2 ml-2 h-4 w-4 rounded-lg focus:outline-none focus:ring-2 ring-purple-600"
+          defaultChecked={numberAllowed}
+          onChange={() => setNumberAllowed(prev => !prev)}
+          />
+          <label className="text-black ml-2">Number</label>
+          <input 
+          type="checkbox"
+          id="charInput"
+          className="mr-2 ml-2 h-4 w-4 rounded-lg focus:outline-none focus:ring-2 ring-purple-600"
+          defaultChecked={charAllowed}
+          onChange={() => setCharAllowed(prev => !prev)}
+          />
+          <label className="text-black ml-2">Char</label>
 
+      </div>
+
+      </>
+  
   );
 }
 export default PasswordApp;
