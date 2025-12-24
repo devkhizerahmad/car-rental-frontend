c3d87ad215dd7ff8b1e74684d44a70cc98f60eef
dev_khizer <devkhizerahmad@gmail.com>
2025-12-24 16:34:07 +0500

    appwrite auth file code

--- src/components/AppWrite/auth.service.js
+++ src/components/AppWrite/auth.service.js
@@ -1,5 +1,6 @@
 import config from "../../config/config";
-import { Client ,Account,ID} from "appwrite";
+import { Client, Account, ID} from "appwrite";
+import UserContext from "../../context/UserContext";
 
 export class AuthService {
   client = new Client();
@@ -7,12 +8,54 @@ export class AuthService {
   constructor() {
     this.client
       .setEndpoint(config.appwriteUrl)
-      .setProject(config.appwriteProjectId); 
+      .setProject(config.appwriteProjectId);
 
     this.account = new Account(this.client);
   }
+  async createAccount(email, password, name) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      console.log("user acc ", userAccount);
+      if (userAccount) {
+        //call another method
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw new Error("Error :", error.message);
+    }
+  }
+  async login(email, password) {
+    try {
+      await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw new Error("Error :", error.message);
+    }
+  }async getCurrentUser() {
+    try {
+      const users = await this.account.get()
+      return users;
+    } catch (error) {
+      throw new Error("Error :", error.message);
+    }
+    return null;
+
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+      // await this.account.deleteSession("current");
+    } catch (error) {
+      throw new Error("Error :", error.message);
+    }
+  }
 }
 
-const authService = new AuthService()
+const authService = new AuthService();
 
-export default  authService;
+export default authService;
