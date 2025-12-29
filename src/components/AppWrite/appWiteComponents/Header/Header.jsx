befc62eac4712ca077b16ea033306f10a12574b0
dev_khizer <devkhizerahmad@gmail.com>
2025-12-29 16:55:05 +0500

    navigation add

--- src/components/AppWrite/appWiteComponents/Header/Header.jsx
+++ src/components/AppWrite/appWiteComponents/Header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Logoutbtn, Logo, Container } from "../index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from ".react-router-dom";
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const navItems = [{ name: "Home", slug: "/", active: true },
+    {name: "Login", slug: "/login", active: !authStatus },
+    {name: "Signup", slug: "/signup", active: !authStatus},
+    {name: "All posts", slug: "/all-posts", active: authStatus},
+    {name: "Add Post", slug: "/add-post", active: authStatus}
+  ];
+  return <div>Header</div>;
 }
 
-export default Header
+export default Header;
--- src/components/AppWrite/appWiteComponents/index.js
+++ src/components/AppWrite/appWiteComponents/index.js
@@ -1,2 +1,12 @@
 import Header from "./footer/Footer";
 import Footer from "./Header/Header";
+import Logo from "../Logo";
+import Container from "./container/Container";
+import Logoutbtn from "./Header/Logoutbtn";
+export {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    Logoutbtn
+}
