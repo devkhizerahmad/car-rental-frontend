ba21ac214ffaab4ad4cb843eb48912295428d0c4
dev_khizer <devkhizerahmad@gmail.com>
2025-12-31 17:48:59 +0500

    feat: add ReactHookForms component for form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import AppWrite from "./components/AppWrite/AppWrite.jsx";
 import AuthTest from "./components/AppWrite/appWiteComponents/AuthTest/AuthTest.jsx";
+import ReactHookForms from "./components/Practice/ReactHookForms.jsx";
 
 function App() {
   return (
     <>
       <h3>Welcome to Car Rental App</h3>
-      <AppWrite />
+      {/* <AppWrite /> */}
       {/* <AuthTest /> */}
+      <ReactHookForms/>
     </>
   );
 }
