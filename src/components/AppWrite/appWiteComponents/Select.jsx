7c70f4412cb8c8b3f2645158461f75bdc491dc9b
dev_khizer <devkhizerahmad@gmail.com>
2025-12-31 13:13:54 +0500

    refactor: restructure Select component to use forwardRef and improve label handling

--- src/components/AppWrite/appWiteComponents/Select.jsx
+++ src/components/AppWrite/appWiteComponents/Select.jsx
@@ -1,4 +1,5 @@
-import React ,{userId} from 'react'
+import { forwardRef } from "react"
+import React, {userId} from 'react'
 
 function Select({
   options,
@@ -6,9 +7,24 @@ function Select({
   className,
   ...props
 }, ref) {
+  const id = userId()
   return (
-    <div>Select</div>
+    <div className='w-full' >
+      {label && <label className='inline-block mb-1' htmlFor={id} >{label}</label>}
+      <select { ...props }
+        id={id}
+        ref={ref} 
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full 
+        ${className}`}
+      >
+        {options?.map((option)=> (
+          <option key={option} value={option} >
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
